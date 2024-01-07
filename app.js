48bdcd92fa5b5422fb7fe837f3ee2ed88f98d7af
Vitaly <vitalic.web@gmail.com>
2024-01-07 15:32:24 +0700

    Add MongoDB

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const port = 3000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+require('dotenv').config();
 
 const app = express();
 app.use(express.json());
@@ -11,17 +14,38 @@ app.use(cors({
   methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH']
 }));
 
-const users = [];
-
 const SECRET_KEY = 'your_secret_key';
+
+const uri = `mongodb+srv://vtlstk:${process.env.DB_PASSWORD}@cluster0.qpufjcu.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+const database = client.db('space-cloud');
+const users = database.collection('users');
+
+async function runDB() {
+  try {
+    await client.connect();
+    console.log('Pinged your deployment. You successfully connected to MongoDB!');
+  } catch (error) {
+    console.error("Error connecting to MongoDB: ", error);
+  }
+}
 
 app.post('/register', async (req, res) => {
   try {
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
     const user = { username: req.body.username, password: hashedPassword };
-    users.push(user);
+    await users.insertOne(user);
     res.status(201).send('User registered');
-  } catch {
+  } catch(err) {
+    console.log(err);
     res.status(500).send();
   }
 });
@@ -43,6 +67,15 @@ app.post('/login', async (req, res) => {
   }
 });
 
-app.listen(port, () => {
-  console.log(`Back running on port ${port}`)
-});
+async function startServer() {
+  try {
+    await runDB();
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+  } catch (error) {
+    console.error('Failed to connect to MongoDB:', error);
+  }
+}
+
+startServer();
