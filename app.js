a003d82cdbc4ae32091d0f9be3a4bdbb140bfc3e
Vitaly <vitalic.web@gmail.com>
2024-03-29 12:18:46 +0700

    Добавлен refreshToken

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const port = 3000;
 require('dotenv').config();
 
 const corsOptions = {
-  origin: '*', // Это позволяет запросы со всех источников
+  origin: '*',
   methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH']
 };
 
@@ -46,7 +46,7 @@ app.post('/register', async (req, res) => {
   try {
     const newUser = new User({ username: req.body.username, password: req.body.password });
     await newUser.save();
-    res.json({ message: 'Registration successful', user: { username: newUser.username } });
+    res.json({ message: 'Registration successful', user: { id: newUser._id, username: newUser.username } });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Registration failed', error: error.message });
@@ -54,17 +54,61 @@ app.post('/register', async (req, res) => {
 });
 
 app.post('/login', (req, res, next) => {
-  passport.authenticate('local', (err, user, info) => {
+  passport.authenticate('local', async (err, user, info) => {
     if (err || !user) {
       return res.status(401).json({ message: 'Authentication failed' });
     }
-    req.logIn(user, (err) => {
+    req.logIn(user, async (err) => {
       if (err) return res.status(500).json({ message: 'Error logging in', error: err });
 
-      const token = jwt.sign({ username: user.username }, process.env.JWT_SECRET, { expiresIn: '24h' });
-      res.json({ message: 'Authentication successful', username: user.username, token });
+      const accessToken = jwt.sign({ username: user.username, userId: user._id.toString() }, process.env.JWT_ACCESS_SECRET, { expiresIn: '15m' });
+      const refreshToken = jwt.sign({ username: user.username, userId: user._id.toString() }, process.env.JWT_REFRESH_SECRET, { expiresIn: '7d' });
+
+      try {
+        await User.findByIdAndUpdate(user._id, { refreshToken: refreshToken }, { new: true });
+
+        res.json({
+          message: 'Authentication successful',
+          userId: user._id,
+          username: user.username,
+          accessToken,
+          refreshToken
+        });
+      } catch (updateError) {
+        console.error('Failed to update user with refreshToken', updateError);
+        res.status(500).json({ message: 'Failed to update user with refreshToken', error: updateError.message });
+      }
     });
   })(req, res, next);
+});
+
+app.post('/token', async (req, res) => {
+  const { refreshToken } = req.body;
+  if (!refreshToken) {
+    return res.status(401).json({ message: 'Refresh Token is required' });
+  }
+  let payload;
+  try {
+    payload = jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET);
+  } catch (e) {
+    return res.status(403).json({ message: 'Invalid Refresh Token' });
+  }
+
+  try {
+    // Поиск пользователя по ID из payload и проверка, совпадает ли refreshToken
+    const user = await User.findById(payload.userId);
+    if (!user || user.refreshToken !== refreshToken) {
+      return res.status(403).json({ message: 'Refresh Token does not match' });
+    }
+
+    // Если всё в порядке, генерируем и отправляем новый access токен
+    const newAccessToken = jwt.sign({ username: user.username, userId: user._id.toString() }, process.env.JWT_ACCESS_SECRET, { expiresIn: '15m' });
+
+    res.json({ accessToken: newAccessToken });
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ message: 'Internal server error' });
+  }
 });
 
 app.get('/logout', (req, res) => {
--- models/User.js
+++ models/User.js
@@ -3,7 +3,8 @@ const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+  password: { type: String, required: true },
+  refreshToken: { type: String, required: false },
 });
 
 UserSchema.pre('save', async function(next) {
